0f24362e6fe60e3f6185fe8571036ca78e591e8e
Aki Balogh <akibalogh@gmail.com>
2013-01-13 12:59:47 -0500

    updates

--- src/main/java/com/genanalytics/WordCloudGen.java
+++ src/main/java/com/genanalytics/WordCloudGen.java
@@ -6,8 +6,6 @@ import java.io.PrintStream;
 import org.neo4j.graphdb.Direction;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.index.Index;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
@@ -21,21 +19,21 @@ public class WordCloudGen
 	private static final String DB_PATH = "/usr/share/neo4j-community-1.8.1/data/articles-categories.db";
 	private static GraphDatabaseService graphDb = new EmbeddedGraphDatabase(DB_PATH);
 	
-	// private static WrappingNeoServerBootstrapper srv = new WrappingNeoServerBootstrapper(graphDb);
+	// private static WrappingNeoServerBootstrapper srv
+	// srv = new WrappingNeoServerBootstrapper(graphDb);
 	// srv.start();
 
 	public static void main( String[] args )
     	{
-    		graphDb = new GraphDatabaseFactory().newEmbeddedDatabase(DB_PATH);
-		// nodeIndex= graphDb.index().forNodes("nodes");
-		// referenceIndex = graphDb.index().forNodes("references");
 		registerShutdownHook();
 
 		Transaction tx = graphDb.beginTx();
 		try
 		{
-			// do something
-		
+			Node n = graphDb.getNodeById(3);
+
+			System.out.println(n.getId());
+	
 			tx.success();
 		}
 		finally
