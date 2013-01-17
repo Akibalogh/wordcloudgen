04e9f1138c76d3be77691ec7e3ad0493dcb77fb1
Aki Balogh <akibalogh@gmail.com>
2013-01-17 06:16:44 -0500

    working transactional index

--- src/main/java/com/genanalytics/WordCloudGen.java
+++ src/main/java/com/genanalytics/WordCloudGen.java
@@ -1,12 +1,17 @@
 package genanalytics;
 
-import java.io.BufferedInputStream;
 import java.io.PrintStream;
+import java.io.IOException;
+import java.io.Console;
+
+import java.text.DecimalFormat;
 
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexManager;
+import org.neo4j.graphdb.index.IndexHits;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
 import org.neo4j.kernel.EmbeddedGraphDatabase;
@@ -17,25 +22,50 @@ import org.neo4j.server.WrappingNeoServerBootstrapper;
 public class WordCloudGen 
 {
 	private static GraphDatabaseService graphDb;	
+	private static Index<Node> index;
 
 	public GraphDatabaseService getGraphDb() { return graphDb; }
+	public Index<Node> getIndex() { return index; }
 
 	public void setGraphDb(GraphDatabaseService graphDb) { this.graphDb = graphDb; }
+	public void setIndex(Index<Node> index) { this.index = index; }
 
-	public Node returnNode(Long id) { return getGraphDb().getNodeById(id); }
-	
+	// public Node returnNode(Long id) { return getGraphDb().getNodeById(id); }
+	// public void setNode(Node node, String key, Object property) { node.setProperty(key, property); }
 	
 	public static void main( String[] args )
-    	{
-		String DB_PATH = "/usr/share/neo4j-community-1.8.1/data/articles-categories.db";
+    	{ 
+		final String DB_PATH = "/usr/share/neo4j-community-1.8.1/data/articles-categories.db";
+		final String PREFIX = "U http://dbpedia.org/resource/";
+		String property = "value";
+		String input = null;
+
+		WordCloudGen w = databaseSetup(DB_PATH, property);
+		// When the index is queried, return results
+		Console console = System.console();
+	
+		while (input != "")
+		{
+			input = console.readLine("> ");
+			System.out.println("I read: '" + PREFIX + input + "'");	
+			// checkIndex(w.getIndex(), property, input);	
+		}
+		System.out.println("Bye!");	
+	
+		//catch(IOException error)
+		//{ error.printStackTrace(); }
+
+	}
+
+
+	public static WordCloudGen databaseSetup(String DB_PATH, String property)
+	{
 		GraphDatabaseFactory neoFactory = new GraphDatabaseFactory();
 		
 		WordCloudGen w = new WordCloudGen();
 		w.setGraphDb(neoFactory.newEmbeddedDatabase(DB_PATH));
 		
 		registerShutdownHook();
-
-		String property = "value";
 
 		Iterable<Node> result = w.getGraphDb().getAllNodes();
 
@@ -45,15 +75,42 @@ public class WordCloudGen
 		for (Node row : result)
 		{ listsize++; }
 
-		System.out.println(listsize);
+		System.out.println("Loaded " + listsize + " entries");
 
+		Transaction tx = w.getGraphDb().beginTx();
 
-		// WORK IN PROGRESS: For each node in the graph, add it to the index
-		for (Node row : result)
-		{ /* System.out.println(row.getId() + " " + row.getProperty(property)); */ }
+		long percentprog = 0L;
 
-		// When the index is queried, return results in the category
+		try
+		{
+			IndexManager indexManager = w.getGraphDb().index();
+			w.setIndex(indexManager.forNodes(property));
 
+			System.out.println("Index set. Loading into index..");
+
+			// For each node in the graph, add it to the index
+			// TODO: Consider using Batch Index load
+			for (Node row : result)
+			{ 
+				if (row.hasProperty(property))
+				{ w.getIndex().add(row, property, row.getProperty(property)); }
+
+				if (row.getId() % 40000 == 0)
+				{ 
+					percentprog = (row.getId() * 100) / (listsize);
+					System.out.println(row.getId() + " of " + listsize + " loaded | " + percentprog + "% loaded");
+					tx.success();
+				}
+
+				// TODO: Probably need to add relationships as well
+			}
+		tx.success();
+		}
+		finally
+		{ tx.finish(); }
+
+		System.out.println("Index loaded");
+		return w;
 	}
 
 	private static void registerShutdownHook()
@@ -62,10 +119,16 @@ public class WordCloudGen
 		{
 			@Override
 			public void run()
-			{
-				graphDb.shutdown();
-			}
+			{ graphDb.shutdown(); }
 		} );
+	}
+
+	public static void checkIndex (Index<Node> index, String property, String input)
+	{
+		IndexHits<Node> hits = index.get(property, input);
+		Node results = hits.getSingle();
+
+		System.out.println(results.getProperty(property));
 	}
 }
 
